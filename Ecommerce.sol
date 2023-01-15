87dca1c85e8ecd69b38e7eb0e2784c33a53fc5bb
mudittiwari <68576920+mudittiwari@users.noreply.github.com>
2023-01-15 23:31:25 +0530

    Add files via upload

--- Ecommerce.sol
+++ Ecommerce.sol
@@ -1,3 +1,20 @@
+////////////////////////// Remaining functionalities ///////////////////////
+// 1. mulitiple items orders
+// 2. get all orders of a particular user
+////////////////////////// added functionalities ///////////////////////
+// 1.create product
+// 2.edit product
+// 3.delete product
+// 4.create user
+// 5.edit user
+// 6.delete user
+// 7.add user family members
+// 8.edit user family members
+// 9.delete user family members
+// 10.add orders
+// 12.remove orders
+// 13.get all products
+// 14.get all users
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.5.16;
 pragma experimental ABIEncoderV2;
@@ -5,19 +22,31 @@ pragma experimental ABIEncoderV2;
 contract Ecommerce {
     mapping(uint256 => Product) public products;
     mapping(string => User) public users;
+    mapping(uint256 => Order) public orders;
     uint256 public counter;
     uint256 public usercounter;
+    uint256 public ordercounter;
+
     constructor() public {
         counter = 1;
-        usercounter=1;
+        usercounter = 1;
+        ordercounter = 1;
     }
 
+    struct Order {
+        string username;
+        string userphone;
+        string product_title;
+        string product_quantity;
+        string date;
+        uint256 price;
+        uint256 id;
+    }
     struct Person {
         string name;
         uint256 age;
         string gender;
     }
-    // orders structure
     struct Product {
         uint256 id;
         string title;
@@ -36,6 +65,8 @@ contract Ecommerce {
         string gender;
         uint256 age;
     }
+
+    /////////////////////////////////////////////////product related functions start/////////////////////
     event productcreated(
         uint256 id,
         string title,
@@ -45,6 +76,16 @@ contract Ecommerce {
         string quantity,
         address author
     );
+    event productedited(
+        uint256 id,
+        string title,
+        uint256 price,
+        string desc,
+        string image,
+        string quantity,
+        address author
+    );
+    event productdeleted(uint256 id, address author);
 
     function createproduct(
         string memory title,
@@ -63,21 +104,92 @@ contract Ecommerce {
         product.quantity = quantity;
         product.created_by = msg.sender;
         emit productcreated(
-            id,
-            title,
-            price,
-            desc,
-            image,
-            quantity,
-            msg.sender
+            product.id,
+            product.title,
+            product.price,
+            product.desc,
+            product.image,
+            product.quantity,
+            product.created_by
         );
         counter++;
     }
 
+    function editproduct(
+        uint256 id,
+        string memory title,
+        uint256 price,
+        string memory desc,
+        string memory quantity,
+        string memory image
+    ) public {
+        Product storage product = products[id];
+        product.title = title;
+        product.price = price;
+        product.desc = desc;
+        product.image = image;
+        product.quantity = quantity;
+        product.created_by = msg.sender;
+        emit productedited(
+            product.id,
+            product.title,
+            product.price,
+            product.desc,
+            product.image,
+            product.quantity,
+            product.created_by
+        );
+    }
 
+    function deleteproduct(uint256 id) public {
+        delete products[id];
+        emit productdeleted(id, msg.sender);
+    }
 
+    function getallproudcts() public view returns (Product[] memory) {
+        Product[] memory prods = new Product[](counter);
+        for (uint i = 0; i < counter; i++) {
+            string memory title=products[i].title;
+            string memory empty='';
+            if (keccak256(abi.encodePacked(title)) != keccak256(abi.encodePacked(empty))) {
+                prods[i]=products[i];
+}
+        }
+        return prods;
+    }
 
-    event usercreated(string name,string phone_number,string address_,string gender,uint256 age,uint256 counter,address author);
+    /////////////////////////////////////////////////product related functions end/////////////////////
+
+    /////////////////////////////////////////////////user related functions start/////////////////////
+
+    event usercreated(
+        string name,
+        string phone_number,
+        string address_,
+        string gender,
+        uint256 age,
+        uint256 counter,
+        address author
+    );
+    event useredited(
+        string name,
+        string phone_number,
+        string address_,
+        string gender,
+        uint256 age,
+        address author
+    );
+    event userdeleted(string phone_number, address author);
+    event member_added(string name, uint256 age, string gender, address author);
+    event cartitemadded(
+        uint256 last_index,
+        uint256 product_id,
+        string phone_number,
+        address author
+    );
+    event removedfromcart(uint256 product_id,string phone_number,address author);
+    event familymemberdeleted(string phone_number,string name,uint256 age,address author);
+    event familymemberedited(string phone_number, string name,uint256 age,string gender,address author);
     function createuser(
         string memory name,
         string memory phone_number,
@@ -91,25 +203,201 @@ contract Ecommerce {
         user.address_ = address_;
         user.age = age;
         user.gender = gender;
-        
-        emit usercreated(name,phone_number,address_,gender,age,usercounter,msg.sender);
+
+        emit usercreated(
+            name,
+            phone_number,
+            address_,
+            gender,
+            age,
+            usercounter,
+            msg.sender
+        );
         usercounter++;
     }
 
-    event member_added(string name,uint256 age,string gender,address author);
-    function addfamilymember(string memory phone_number,string memory name,uint256 age,string memory gender) public
-    {
-        User storage user=users[phone_number];
-        Person memory person; 
-        person.age=age;
-        person.gender=gender;
-        person.name=name;
-        user.family_members.push(person);
-        emit member_added(user.family_members[0].name,user.family_members[0].age,user.family_members[0].gender,msg.sender);
+    function edituser(
+        string memory old_phone_number,
+        string memory new_name,
+        string memory new_address_,
+        string memory new_gender,
+        uint256 new_age
+    ) public {
+        User storage user = users[old_phone_number];
+        user.name = new_name;
+        user.address_ = new_address_;
+        user.age = new_age;
+        user.gender = new_gender;
+
+        emit useredited(
+            user.name,
+            user.phone_number,
+            user.address_,
+            user.gender,
+            user.age,
+            msg.sender
+        );
     }
-    function getfamilymembers(string memory phone_number) public view returns (Person[] memory)
+
+    function deleteuser(string memory phone_number) public {
+        delete users[phone_number];
+        emit userdeleted(phone_number, msg.sender);
+    }
+
+    function addtocart(uint256 product_id, string memory phone_number) public {
+        User storage user = users[phone_number];
+        user.cartitems.push(product_id);
+        uint256 cart_len = user.cartitems.length;
+        uint256 last_index = cart_len - 1;
+        emit cartitemadded(
+            last_index,
+            user.cartitems[last_index],
+            user.phone_number,
+            msg.sender
+        );
+    }
+    function removefromcart(uint256 product_id,string memory phone_number) public{
+        User storage user=users[phone_number];
+        int index=-1;
+        for (uint i = 0; i < user.cartitems.length; i++) {
+            if(user.cartitems[i]==product_id)
+                index=int(i);           
+        }
+        if(index==-1)
+            return ;
+        for (uint i = uint(index); i<user.cartitems.length-1; i++){
+            user.cartitems[i] = user.cartitems[i+1];
+        }
+        user.cartitems.pop();
+        emit removedfromcart(product_id,phone_number,msg.sender);
+    }
+
+    function addfamilymember(
+        string memory phone_number,
+        string memory name,
+        uint256 age,
+        string memory gender
+    ) public {
+        User storage user = users[phone_number];
+        Person memory person;
+        person.age = age;
+        person.gender = gender;
+        person.name = name;
+        user.family_members.push(person);
+        emit member_added(
+            user.family_members[0].name,
+            user.family_members[0].age,
+            user.family_members[0].gender,
+            msg.sender
+        );
+    }
+    function deletefamilymember(string memory phone_number,string memory name,uint256 age)
+    public{
+        User storage user = users[phone_number];
+        int index=-1;
+        for (uint i = 0; i < user.family_members.length; i++) {
+            if(keccak256(abi.encodePacked(user.family_members[i].name)) == keccak256(abi.encodePacked(name)) &&keccak256(abi.encodePacked(user.family_members[i].age)) == keccak256(abi.encodePacked(age)))
+                index=int(i);           
+        }
+        if(index==-1)
+            return ;
+        for (uint i = uint(index); i<user.family_members.length-1; i++){
+            user.family_members[i] = user.family_members[i+1];
+        }
+        user.family_members.pop();
+        emit familymemberdeleted(phone_number,name,age,msg.sender);
+    }
+
+
+    function editfamilymember(string memory phone_number,string memory oldname,string memory newname,uint256 oldage,uint256 newage,string memory newgender)
+    public{
+        User storage user = users[phone_number];
+        int index=-1;
+        for (uint i = 0; i < user.family_members.length; i++) {
+            if(keccak256(abi.encodePacked(user.family_members[i].name)) == keccak256(abi.encodePacked(oldname)) &&keccak256(abi.encodePacked(user.family_members[i].age)) == keccak256(abi.encodePacked(oldage)))
+                index=int(i);           
+        }
+        if(index==-1)
+            return ;
+        Person storage per=user.family_members[uint(index)];
+        per.age=newage;
+        per.gender=newgender;
+        per.name=newname;
+        emit familymemberedited(phone_number, per.name, per.age,per.gender, msg.sender);
+    }
+    function getallusers() public view returns (User[] memory) {
+        Product[] memory users_ = new User[](usercounter);
+        for (uint i = 0; i < usercounter; i++) {
+            string memory name=users[i].name;
+            string memory empty='';
+            if (keccak256(abi.encodePacked(title)) != keccak256(abi.encodePacked(empty))) {
+                users_[i]=users[i];
+}
+        }
+        return users_;
+    }
+    function getfamilymembers(string memory phone_number)
+        public
+        view
+        returns (Person[] memory)
+    {
+        User storage user = users[phone_number];
+        return user.family_members;
+    }
+
+    function getcartitems(string memory phone_number) public
+        view
+        returns (uint256[] memory)
     {
         User storage user=users[phone_number];
-        return user.family_members;
+        return user.cartitems;
+    }
+
+    /////////////////////////////////////////////////user related functions end/////////////////////
+    event ordercreated(
+        uint256 id,
+        string username,
+        string userphone,
+        string product_title,
+        string product_quantity,
+        string date,
+        uint256 price,
+        address author
+    );
+    event orderdeleted(uint256 id,address author);
+
+    function createorder(
+        string memory username,
+        string memory userphone,
+        string memory product_title,
+        string memory product_quantity,
+        string memory date,
+        uint256 price
+    ) public {
+        Order storage order = orders[ordercounter];
+        order.id = ordercounter;
+        order.username = username;
+        order.userphone = userphone;
+        order.product_title = product_title;
+        order.product_quantity = product_quantity;
+        order.date = date;
+        order.price = price;
+        emit ordercreated(
+            order.id,
+            order.username,
+            order.userphone,
+            order.product_title,
+            order.product_quantity,
+            order.date,
+            order.price,
+            msg.sender
+        );
+        ordercounter++;
+    }
+
+    function deleteorder(uint256 order_id)
+    public{
+        delete orders[order_id];
+        emit orderdeleted(order_id, msg.sender);
     }
 }
--- ecommerce.js
+++ ecommerce.js
@@ -12,13 +12,30 @@ contract('Ecommerce', ([deployer, author, tipper]) => {
     }
     );
     describe('deployment', () => {
-        let product,productcounter,user,usercounter,member;
+        let product, productcounter, user,usercounter, member, order,prod,dprod,euser,duser,citemadd,deleteorder,ordercounter,removefromcart,removemember,editmember;
         before(async () => {
             product = await ecommerce.createproduct('sugar', 200, "this is the first product", "200", "test image", { from: author });
-            user=await ecommerce.createuser('mudit tiwari',"94135655045","scheme 8","male",18,{ from: author });
-            member=await ecommerce.addfamilymember('94135655045',"nimit",10,"male",{ from: author });
+            prod=await ecommerce.editproduct(1,'wheat',100,"wheattt","100 Kg","final image",{from:author});
+            user = await ecommerce.createuser('mudit tiwari', "94135655045", "scheme 8", "male", 18, { from: author });
+            euser=await ecommerce.edituser("94135655045",'mudit', "scheme 10", "male", 20, { from: author });
+            member = await ecommerce.addfamilymember('94135655045', "nimit", 10, "male", { from: author });
+            editmember=await ecommerce.editfamilymember('94135655045',"nimit", "nimit2",10,12,"male", { from: author })
+            citemadd=await ecommerce.addtocart(1,"94135655045",{from:author});
+            
+            order = await ecommerce.createorder("mudit",
+                "11",
+                "wheat",
+                "100 kg",
+                "11:11:2022",
+                500,{ from: author })
+            deleteorder=await ecommerce.deleteorder(1,{from:author});
+            removefromcart=await ecommerce.removefromcart(1,"94135655045",{from:author});
+            removemember=await ecommerce.deletefamilymember("94135655045","nimit2",12,{from:author});
+            dprod=await ecommerce.deleteproduct(1,{from:author});
+            duser=await ecommerce.deleteuser("94135655045",{from:author});
             productcounter = await ecommerce.counter();
-            usercounter=await ecommerce.usercounter();
+            usercounter = await ecommerce.usercounter();
+            ordercounter=await ecommerce.ordercounter();
 
         }
         );
@@ -32,7 +49,7 @@ contract('Ecommerce', ([deployer, author, tipper]) => {
         }
 
         );
-        it('creates posts', async () => {
+        it('creates product', async () => {
 
             // assert.equal(postcounter, 1);
             let event = product.logs[0].args;
@@ -45,31 +62,137 @@ contract('Ecommerce', ([deployer, author, tipper]) => {
             assert.equal(event.image, 'test image', 'image is correct');
         }
         );
-
-        it('creates user', async () => {
+        it('edit product', async () => {
 
             // assert.equal(postcounter, 1);
+            let event = prod.logs[0].args;
+            assert.equal(event.id.toNumber(), productcounter.toNumber() - 1, 'id is correct');
+            assert.equal(event.title, 'wheat', 'Title is correct');
+            assert.equal(event.author, author, 'author is correct');
+            assert.equal(event.price, 100, 'price is correct');
+            assert.equal(event.desc, "wheattt", 'description is correct');
+            assert.equal(event.quantity, "100 Kg", 'quantity is correct');
+            assert.equal(event.image, 'final image', 'image is correct');
+        }
+        );
+
+        it('delete product', async () => {
+
+            // assert.equal(postcounter, 1);
+            let event = dprod.logs[0].args;
+            assert.equal(event.id.toNumber(), productcounter.toNumber() - 1, 'id is correct');
+            assert.equal(event.author, author, 'author is correct');
+        }
+        );
+
+
+        it('creates user', async () => {
             let event = user.logs[0].args;
-            assert.equal(event.counter.toNumber(), usercounter.toNumber()-1, 'user counter is correct');
+            assert.equal(event.counter.toNumber(), usercounter.toNumber() - 1, 'user counter is correct');
             assert.equal(event.name, 'mudit tiwari', 'name is correct');
             assert.equal(event.author, author, 'author is correct');
-            assert.equal(event.phone_number, 94135655045, 'phone number is correct');
+            assert.equal(event.phone_number, "94135655045", 'phone number is correct');
             assert.equal(event.address_, "scheme 8", 'address is correct');
             assert.equal(event.gender, "male", 'gender is correct');
             assert.equal(event.age, 18, 'age is correct');
         }
         );
 
+        it('edit user', async () => {
+            let event = euser.logs[0].args;
+            assert.equal(event.name, 'mudit', 'name is correct');
+            assert.equal(event.author, author, 'author is correct');
+            assert.equal(event.phone_number, "94135655045", 'phone number is correct');
+            assert.equal(event.address_, "scheme 10", 'address is correct');
+            assert.equal(event.gender, "male", 'gender is correct');
+            assert.equal(event.age, 20, 'age is correct');
+        }
+        );
+
+        it('add to cart', async () => {
+
+            let event = citemadd.logs[0].args;
+            assert.equal(event.author, author, 'author is correct');
+            assert.equal(event.phone_number, "94135655045", 'phone number is correct');
+            assert.equal(event.last_index, 0, 'last index is correct');
+            assert.equal(event.product_id, productcounter.toNumber() - 1, 'product id is correct');
+        }
+        );
+
+        it('remove from cart', async () => {
+
+            let event = removefromcart.logs[0].args;
+            assert.equal(event.author, author, 'author is correct');
+            assert.equal(event.phone_number, "94135655045", 'phone number is correct');
+            assert.equal(event.product_id, 1, 'product id is correct');
+        }
+        );
+        it('delete user', async () => {
+
+            let event = duser.logs[0].args;
+            assert.equal(event.author, author, 'author is correct');
+            assert.equal(event.phone_number, "94135655045", 'phone number is correct');
+        }
+        );
+
+        
+
         it('add family member', async () => {
 
             // assert.equal(postcounter, 1);
             let event = member.logs[0].args;
-            
+
             assert.equal(event.name, 'nimit', 'name is correct');
             assert.equal(event.author, author, 'author is correct');
-            
+
             assert.equal(event.gender, "male", 'gender is correct');
             assert.equal(event.age, 10, 'age is correct');
+        }
+        );
+        it('edit family member', async () => {
+
+            // assert.equal(postcounter, 1);
+            let event = editmember.logs[0].args;
+
+            assert.equal(event.name, 'nimit2', 'name is correct');
+            assert.equal(event.phone_number, '94135655045', 'name is correct');
+            assert.equal(event.author, author, 'author is correct');
+
+            assert.equal(event.gender, "male", 'gender is correct');
+            assert.equal(event.age, 12, 'age is correct');
+        }
+        );
+        it('remove family member', async () => {
+
+            // assert.equal(postcounter, 1);
+            let event = removemember.logs[0].args;
+
+            assert.equal(event.name, 'nimit2', 'name is correct');
+            assert.equal(event.author, author, 'author is correct');
+
+            assert.equal(event.phone_number, "94135655045", 'phone is correct');
+            assert.equal(event.age, 12, 'age is correct');
+        }
+        );
+
+        it('create order', async () => {
+            // assert.equal(postcounter, 1);
+            let event = order.logs[0].args;
+
+            assert.equal(event.username, 'mudit', 'username is correct');
+            assert.equal(event.userphone, "11", 'userphone is correct');
+            assert.equal(event.author,author,"author is correct")
+            assert.equal(event.product_title, "wheat", 'product title is correct');
+            assert.equal(event.date, "11:11:2022", 'date is correct');
+            assert.equal(event.price, 500, 'price is correct');
+        }
+        );
+        it('delete order', async () => {
+
+            // assert.equal(postcounter, 1);
+            let event = order.logs[0].args;
+            assert.equal(event.id.toNumber(), ordercounter.toNumber() - 1, 'id is correct');
+            assert.equal(event.author, author, 'author is correct');
         }
         );
 
